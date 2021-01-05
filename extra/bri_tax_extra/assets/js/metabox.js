d571edb356f848015299cf702cc2c6af9aeaaf5d
Ravil <foragetonly800@mail.ru>
2021-01-05 07:19:34 +0300

    Описал функционал добавления медиа файла

--- extra/bri_tax_extra/assets/js/metabox.js
+++ extra/bri_tax_extra/assets/js/metabox.js
@@ -3,8 +3,30 @@
 	
 	$( document ).ready( () => {
 
-		let a = 2;
+		$( '.briz-media-button' ).on( 'click', evt => {
+			let args = {
+				title: 'Insert a media',
+				library: { type: 'image' },
+				multiple: false,
+				button: { text: 'Insert' }
+			};
 
-		console.log( a );
+			let win = wp.media( args );
+
+			win.on( 'select', () => {
+				let res = win.state().get( 'selection' ).first();
+				console.log( res );
+
+				let el = $( evt.target );
+				let imagePlace = el.parent().find( '.briz-media-place' );
+
+				$( '<img />', {
+					src: res.attributes.url
+				} ).appendTo( imagePlace );
+			} );
+
+			win.open();
+		} );
+
 	} );
 } )( jQuery );
