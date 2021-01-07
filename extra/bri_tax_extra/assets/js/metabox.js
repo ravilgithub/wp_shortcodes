7d5a383fb390b499df584d6e44fe2acc875572d1
Ravil <foragetonly800@mail.ru>
2021-01-07 05:39:49 +0300

    Refactoring

--- extra/bri_tax_extra/assets/js/metabox.js
+++ extra/bri_tax_extra/assets/js/metabox.js
@@ -4,25 +4,35 @@
 	$( document ).ready( () => {
 
 		$( '.briz-media-button' ).on( 'click', evt => {
-			let args = {
+			let args, win, btn;
+			
+			btn = $( evt.target );
+			
+			args = {
 				title: 'Insert a media',
 				library: { type: 'image' },
 				multiple: false,
 				button: { text: 'Insert' }
 			};
 
-			let win = wp.media( args );
+			win = wp.media( args );
 
 			win.on( 'select', () => {
-				let res = win.state().get( 'selection' ).first();
-				console.log( res );
+				let res, imagePlace, img;
+				
+				res = win
+				        .state()
+				        .get( 'selection' )
+				        .first();
 
-				let el = $( evt.target );
-				let imagePlace = el.parent().find( '.briz-media-place' );
-
-				$( '<img />', {
+				img = $( '<img />', {
 					src: res.attributes.url
-				} ).appendTo( imagePlace );
+				} );
+
+				btn
+				  .parent()
+				  .find( '.briz-media-place' )
+				  .html( img );
 			} );
 
 			win.open();
