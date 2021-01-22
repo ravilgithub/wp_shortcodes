583b34f30a9a158cd20fecbd772098adda830af7
Ravil <foragetonly800@mail.ru>
2021-01-22 03:48:11 +0300

    Работа с кнопками добавления/удаления медиа файлов

--- extra/bri_tax_extra/assets/js/metabox.js
+++ extra/bri_tax_extra/assets/js/metabox.js
@@ -2,12 +2,38 @@
 	'use strict';
 	
 	$( document ).ready( () => {
+		let add_action_txt,
+		    edit_action_txt;
+
+		let getBtnActionTxt = ( btn, action ) => {
+			if ( 'del' === action ) {
+				btn = btn.parent().find( '.briz-media-button' );
+				console.log( btn );
+			}
+			add_action_txt = btn.data( 'add-text' );
+			edit_action_txt = btn.data( 'edit-text' );
+		};
+
+		let btnHandler = ( btn, action ) => {
+			getBtnActionTxt( btn, action );
+
+			if ( 'add' === action ) {
+				console.log( edit_action_txt );
+			} else {
+				console.log( add_action_txt );
+			}
+		};
+
+		$( '.briz-del-media-button' ).on( 'click', evt => {
+			let btn = $( evt.target );
+			btnHandler( btn, 'del' );
+		} );
 
 		$( '.briz-media-button' ).on( 'click', evt => {
 			let args, wpm, btn;
-			
+
 			btn = $( evt.target );
-			
+
 			args = {
 				title: btn.data( 'title' ),
 				library: { type: btn.data( 'library-type' ) },
@@ -39,7 +65,10 @@
 				btn
 					.parent()
 					.find( '.briz-media-place' )
-						.html( imgs )
+						.html( () => { 
+							btnHandler( btn, 'add' );
+							return imgs;
+						} )
 						.end()
 					.find( 'input[type=hidden]' )
 						.attr( 'value', JSON.stringify( ids ) );
