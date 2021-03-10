a3c68d717e746aaaf2ce161df0e22bfaba27e176
ravilgithub <foragetonly800@mail.ru>
2021-03-10 15:21:06 +0300

    Documentation, Clear

--- inc/shortcode_bri_spacer.php
+++ inc/shortcode_bri_spacer.php
@@ -1,115 +1,133 @@
 <?php
+namespace Bri_Shortcodes;
+
 /**
  * Класс реализует шорткод "bri_spacer",
  * который позволяет формировать
  * HTML разметку элемента.
  *
- * Доступные атрибуты:
- * @class  - дополнительные классы.
- * @height - высота элемента.
- * @back   - CSS цвет фона.
- * 
+ * @property String $name - Имя шорткода.
+ * @property Array $assets - {
+ *  ... Array $deps - Массив идентификаторов других стилей/скриптов,
+ *                    от которых зависит подключаемый файл стилей/скрипт.
+ *                    Указанные тут стили, будут подключены до текущего.
+ *  ... String $ver - Строка определяющая версию стилей/скрипта.
+ * }
+ * @property Array $inline_styles - инлайн стили образованные из атрибутов шорткода.
+ * @property Integer $n - порядковый номер шордкода.
+ * @property Array $default_atts {
+ *  Доступные атрибуты:
+ *   @class  - дополнительные классы.
+ *   @height - высота элемента.
+ *   @back   - CSS цвет фона.
+ * }
+ *
  * Пример:
- * [bri_spacer height="50px" back="#fff"][/bri_spacer]
+ * [bri_spacer
+ *  height="50px"
+ *  back="#add76f"
+ * ][/bri_spacer]
+ *
+ * @since 0.0.1
+ * @author Ravil
  */
-
-namespace Bri_Shortcodes;
-
 class Bri_Spacer_Shortcode extends Bri_Shortcodes {
-  public $name   = 'bri_spacer';
-  public $assets = [
-    'css' => [
-      'bri_spacer' => [
-        'deps' => [],
-        'ver'  => '1.0.0'
-      ],
-    ]
-  ];
-  public $inline_styles = array();
-  public static $n      = 1;
-  public $default_atts  = array(
-    'class'  => '',
-    'height' => '20px',
-    'back'   => 'inherit',
-  );
+	public $name   = 'bri_spacer';
+	public $assets = [
+		'css' => [
+			'bri_spacer' => [
+				'deps' => [],
+				'ver'  => '1.0.0'
+			],
+		],
+
+		'js' => []
+	];
+	public $inline_styles = [];
+	public static $n      = 1;
+	public $default_atts  = [
+		'class'  => '',
+		'height' => '20px',
+		'back'   => 'inherit',
+	];
 
 
-  /**
-   * Call parent __construct function.
-   *
-   * Вызываем родительскую функцию __construct.
-   *
-   * @param Object $obj - может принять другой объект в котором будет обрабатываться шорткод.
-   *
-   * @return void.
-   * @since  0.0.1
-   * @author Ravil
-   */
-  public function __construct( $obj = '' ) {
-    parent::__construct( $obj );
-  }
+	/**
+	 * Call parent __construct function.
+	 *
+	 * Вызываем родительскую функцию __construct.
+	 *
+	 * @param Object $obj - может принять другой объект в котором будет обрабатываться шорткод.
+	 *
+	 * @return void.
+	 * @since  0.0.1
+	 * @author Ravil
+	 */
+	public function __construct( $obj = '' ) {
+		parent::__construct( $obj );
+	}
 
 
-  /**
-   * List shortcode callback.
-   *
-   * Функция обработчик шорткода "bri_spacer".
-   *
-   * @param Array  $atts    - атрибуты переданные в шорткод.
-   * @param String $content - контент переданный между парными тегами шорткода.
-   * @param String $tag     - имя шорткода указанный первым параметром в фукции "add_shortcode".
-   *
-   * @return String HTML    - разметка сформированного шорткода.
-   * @since  0.0.1
-   * @author Ravil
-   */
-  public function shortcode_bri_spacer( $atts, $content, $tag ) {
-    $default_class = $this->get_full_name();
-    $id            = $default_class . '_' . self::$n++;
+	/**
+	 * List shortcode callback.
+	 *
+	 * Функция обработчик шорткода "bri_spacer".
+	 *
+	 * @param Array  $atts    - атрибуты переданные в шорткод.
+	 * @param String $content - контент переданный между парными тегами шорткода.
+	 * @param String $tag     - имя шорткода указанный первым параметром в фукции "add_shortcode".
+	 *
+	 * @return String HTML    - разметка сформированного шорткода.
+	 * @since  0.0.1
+	 * @author Ravil
+	 */
+	public function shortcode_bri_spacer( $atts, $content, $tag ) {
+		$default_class = $this->get_full_name();
+		$id            = $default_class . '_' . self::$n++;
 
-    $content = wp_kses( $content, 'post' );
+		$content = wp_kses( $content, 'post' );
 
-    $atts = $this->prepare_atts( $atts );
-    
-    $atts[ 'class' ] .= ( ! empty( $atts[ 'class' ] ) ) ? " $default_class" : $default_class;
+		$atts = $this->prepare_atts( $atts );
 
-    if ( ! empty( $atts[ 'height' ] ) ) {
-      // { height }
-      $this->add_inline_styles( '', 'height', $atts[ 'height' ] );
-    }
+		$atts[ 'class' ] .= ( ! empty( $atts[ 'class' ] ) ) ? " $default_class" : $default_class;
 
-    if ( ! empty( $atts[ 'back' ] ) ) {
-      // { background-color }
-      $this->add_inline_styles( '', 'background-color', $atts[ 'back' ] );
-    }
+		if ( ! empty( $atts[ 'height' ] ) ) {
+			// { height }
+			$this->add_inline_styles( '', 'height', $atts[ 'height' ] );
+		}
 
-    $this->add_shortcode_style( $id, $atts );
+		if ( ! empty( $atts[ 'back' ] ) ) {
+			// { background-color }
+			$this->add_inline_styles( '', 'background-color', $atts[ 'back' ] );
+		}
 
-    return $this->display_image( $content, $atts, $id );
-  }
+		$this->add_shortcode_style( $id, $atts );
+
+		return $this->display_image( $content, $atts, $id );
+	}
 
 
-  /**
-   * Display Blockquote.
-   *
-   * Формирует разметку элемента.
-   *
-   * @param String $content - текст элемента.
-   * @param Array  $atts    - атрибуты переданные в шорткод.
-   * @param String $id      - атрибут элемента.
-   *
-   * @return String HTML    - разметка элемента.
-   * @since  0.0.1
-   * @author Ravil
-   */
-  public function display_image( $content, $atts, $id ) {
+	/**
+	 * Display Blockquote.
+	 *
+	 * Формирует разметку элемента.
+	 *
+	 * @param String $content - текст элемента.
+	 * @param Array  $atts    - атрибуты переданные в шорткод.
+	 * @param String $id      - атрибут элемента.
+	 *
+	 * @return String HTML    - разметка элемента.
+	 * @since  0.0.1
+	 * @author Ravil
+	 */
+	public function display_image( $content, $atts, $id ) {
 
-    $lang_domain = apply_filters( 'bri_shortcode_lang_domain', $this->lang_domain );
+		$lang_domain = apply_filters( 'bri_shortcode_lang_domain', $this->lang_domain );
 
-    ob_start();
+		ob_start();
 ?>
-    <div id="<?php echo $id ?>" class="<?php echo $atts[ 'class' ] ?>"></div>
+		<div id="<?php echo $id ?>" class="<?php echo $atts[ 'class' ] ?>"></div>
 <?php
-    return trim( ob_get_clean() );
-  }
+		return trim( ob_get_clean() );
+	}
 }
