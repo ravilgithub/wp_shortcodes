c7204b96b1572b6689cba7a0e8268e4db0439a0b
Ravil <foragetonly800@mail.ru>
2021-03-14 17:06:12 +0300

    Documentation, Clear

--- extra/bri_tax_extra/meta/meta_opts.php
+++ extra/bri_tax_extra/meta/meta_opts.php
@@ -1,97 +1,125 @@
 <?php
+/**
+ * Array of parameters for creating metaboxes with fields.
+ *
+ * Массив параметров для создания мета блоков с полями.
+ *
+ * @see ~/extra/bri_tax_extra/meta_boxes.php
+ *      Meta_boxes::__construct()
+ *
+ * @param Array $opts {
+ *  @type Array $taxonomy_name {
+ *   @type Array $term_name {
+ *    @type Array $fields {
+ *     @type Array $option_name {
+ *      @type String $type        - Тип мета поля.
+ *                                  Required.
+ *      @type String $title       - Название мета поля.
+ *      @type String $desc        - Описание мета поля.
+ *      @type String/Array $value - Значение мета поля по умолчанию.
+ *                                  Required.
+ *                                  Default: ''
+ *      @type Array $options      - Параметры мета поля.
+ *     }
+ *    }
+ *   }
+ *  }
+ * }
+ *
+ */
 
 $opts = [
 	'category' => [
 		'portfolio' => [
 			'fields' => [
 				'option_1' => [
-					'type' => 'text',
+					'type'  => 'text',
 					'title' => 'Опция 1',
-					'desc' => 'Описание опции 1',
+					'desc'  => 'Описание опции 1',
 					'value' => 'Значение по умолчанию опции 1'
 				],
 				'option_2' => [
-					'type' => 'textarea',
+					'type'  => 'textarea',
 					'title' => 'Опция 2',
-					'desc' => 'Описание опции 2',
+					'desc'  => 'Описание опции 2',
 					'value' => 'Значение по умолчанию опции 2'
 				],
 				'option_3' => [
-					'type' => 'color',
+					'type'  => 'color',
 					'title' => 'Опция 3',
-					'desc' => 'Описание опции 3',
+					'desc'  => 'Описание опции 3',
 					'value' => '#00cccc'
 				],
 				'option_4' => [
-					'type' => 'number',
+					'type'  => 'number',
 					'title' => 'Опция 4',
-					'desc' => 'Описание опции 4',
+					'desc'  => 'Описание опции 4',
 					'value' => 5,
 					'options' => [
 						'step' => 2,
-						'min' => 0,
-						'max' => 10
+						'min'  => 0,
+						'max'  => 10
 					]
 				],
 				'option_5' => [
-					'type' => 'select',
+					'type'  => 'select',
 					'title' => 'Опция 5',
-					'desc' => 'Описание опции 5',
+					'desc'  => 'Описание опции 5',
 					'value' => 'red',
 					'options' => [
-						'red' => 'Red',
+						'red'   => 'Red',
 						'green' => 'Green',
-						'blue' => 'Blue'
+						'blue'  => 'Blue'
 					]
 				],
 				'option_6' => [
-					'type' => 'checkbox',
+					'type'  => 'checkbox',
 					'title' => 'Опция 6',
-					'desc' => 'Описание опции 6',
+					'desc'  => 'Описание опции 6',
 					'value' => [
 						'green',
 						'red'
 					],
 					'options' => [
-						'red' => 'Red',
+						'red'   => 'Red',
 						'green' => 'Green',
-						'blue' => 'Blue'
+						'blue'  => 'Blue'
 					]
 				],
 				'option_7' => [
-					'type' => 'range',
+					'type'  => 'range',
 					'title' => 'Опция 7',
-					'desc' => 'Описание опции 7',
+					'desc'  => 'Описание опции 7',
 					'value' => '5',
 					'options' => [
 						'step' => 2,
-						'min' => 0,
-						'max' => 10
+						'min'  => 0,
+						'max'  => 10
 					]
 				],
 				'option_8' => [
-					'type' => 'radio',
+					'type'  => 'radio',
 					'title' => 'Опция 8',
-					'desc' => 'Описание опции 8',
+					'desc'  => 'Описание опции 8',
 					'value' => 'green',
 					'options' => [
-						'red' => 'Red',
+						'red'   => 'Red',
 						'green' => 'Green',
-						'blue' => 'Blue'
+						'blue'  => 'Blue'
 					]
 				],
 				'option_9' => [
-					'type' => 'url',
-					'title' => 'Опция 9',
-					'desc' => 'Описание опции 9',
-					'value' => 'https://yandex.ru',
-					'pattern' => 'https://.*',
+					'type'     => 'url',
+					'title'    => 'Опция 9',
+					'desc'     => 'Описание опции 9',
+					'value'    => 'https://yandex.ru',
+					'pattern'  => 'https://.*',
 					'required' => true
 				],
 				'option_10' => [
-					'type' => 'wp_editor',
+					'type'  => 'wp_editor',
 					'title' => 'Опция 10',
-					'desc' => 'Описание опции 10',
+					'desc'  => 'Описание опции 10',
 					'value' => '<h1>Title</h1>',
 					'options' => [
 						'editor_class'     => 'editor-class',
@@ -108,39 +136,39 @@ $opts = [
 					]
 				],
 				'option_11' => [
-					'type' => 'media_button',
+					'type'  => 'media_button',
 					'title' => 'Опция 11',
-					'desc' => 'Описание опции 11',
+					'desc'  => 'Описание опции 11',
 					'value' => '',
 					'options' => [
-						'title' => 'Insert a media',
-						'library' => [ 'type' => 'image' ],
+						'title'    => 'Insert a media',
+						'library'  => [ 'type' => 'image' ],
 						'multiple' => 0,
-						'button' => [ 'text' => 'Insert' ]
+						'button'   => [ 'text' => 'Insert' ]
 					]
 				],
 				'option_12' => [
-					'type' => 'media_button',
+					'type'  => 'media_button',
 					'title' => 'Опция 12',
-					'desc' => 'Описание опции 12',
+					'desc'  => 'Описание опции 12',
 					'value' => '',
 					'options' => [
-						'title' => 'Insert a media',
-						'library' => [ 'type' => 'audio' ],
+						'title'    => 'Insert a media',
+						'library'  => [ 'type' => 'audio' ],
 						'multiple' => 1,
-						'button' => [ 'text' => 'Insert' ]
+						'button'   => [ 'text' => 'Insert' ]
 					]
 				],
 				'option_13' => [
-					'type' => 'media_button',
+					'type'  => 'media_button',
 					'title' => 'Опция 13',
-					'desc' => 'Описание опции 13',
+					'desc'  => 'Описание опции 13',
 					'value' => '',
 					'options' => [
-						'title' => 'Insert a media',
-						'library' => [ 'type' => 'video' ],
+						'title'    => 'Insert a media',
+						'library'  => [ 'type' => 'video' ],
 						'multiple' => 1,
-						'button' => [ 'text' => 'Insert' ]
+						'button'   => [ 'text' => 'Insert' ]
 					]
 				],
 			],
@@ -148,15 +176,15 @@ $opts = [
 		'about' => [
 			'fields' => [
 				'option_1' => [
-					'type' => 'text',
+					'type'  => 'text',
 					'title' => 'Опция 1',
-					'desc' => 'Описание опции 1',
+					'desc'  => 'Описание опции 1',
 					'value' => 'Значение по умолчанию опции 1'
 				],
 				'option_2' => [
-					'type' => 'textarea',
+					'type'  => 'textarea',
 					'title' => 'Опция 2',
-					'desc' => 'Описание опции 2',
+					'desc'  => 'Описание опции 2',
 					'value' => 'Значение по умолчанию опции 2'
 				],
 			],
