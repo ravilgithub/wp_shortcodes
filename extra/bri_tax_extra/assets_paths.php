b01c4cad0f980fb94014b8d8ad5b35329763f76b
ravilgithub <foragetonly800@mail.ru>
2021-03-15 17:09:05 +0300

    Change - Перенёс массив параметров, подключаемых стилей и скриптов относящихся к шорткоду 'bri_tax' в отдельный файл

--- extra/bri_tax_extra/assets_paths.php
+++ extra/bri_tax_extra/assets_paths.php
@@ -1,15 +1,23 @@
 <?php
-	$plugin_path = plugin_dir_path( __FILE__ );
+$assets = [
+	'css' => [
+		/************ TMPL CSS ************/
+		'portfolio' => [
+			'id'   => 'portfolio-tmpl-css',
+			'src'  => PLUGIN_URL . 'extra/bri_tax_extra/assets/css/portfolio.min.css',
+			'deps' => [ 'bri-bootstrap-css', 'bri-magnific-popup-css', 'bri-fontawesome-css' ],
+			'ver'  => '1.0.0'
+		]
+	],
 
-	$assets = [
-		'styles' => [
-
-			'portfolio' => [
-				'id'  => 'portfolio-tmpl-style',
-				'src' => $plugin_path . 'extra/bri_tax_extra/assets_paths.php',
-				'deps' => [ 'bri-bootstrap', 'bri-magnific-popup' ],
-				'ver'  => '1.0.0'
-			]
-		],
-		'scripts' => [ ... ]
-	];
+	'js' => [
+		/************ TMPL SCRIPTS ************/
+		'portfolio' => [
+			'id'        => 'portfolio-tmpl-js',
+			'src'       => PLUGIN_URL . 'extra/bri_tax_extra/assets/js/portfolio.js',
+			'deps'      => [ 'jquery', 'imagesloaded', 'bri-bootstrap-js', 'bri-magnific-popup-js', 'bri-isotop-js' ],
+			'ver'       => '1.0.0',
+			'in_footer' => true
+		]
+	]
+];
--- inc/shortcode_bri_tax.php
+++ inc/shortcode_bri_tax.php
@@ -181,29 +181,7 @@ class Bri_Tax_Shortcode extends Bri_Shortcodes {
 	 * @author Ravil
 	 */
 	public function register_assets() {
-		$assets = [
-			'css' => [
-				/************ TMPL CSS ************/
-				'portfolio' => [
-					'id'   => 'portfolio-tmpl-css',
-					'src'  => PLUGIN_URL . 'extra/bri_tax_extra/assets/css/portfolio.min.css',
-					'deps' => [ 'bri-bootstrap-css', 'bri-magnific-popup-css', 'bri-fontawesome-css' ],
-					'ver'  => '1.0.0'
-				]
-			],
-
-			'js' => [
-				/************ TMPL SCRIPTS ************/
-				'portfolio' => [
-					'id'   => 'portfolio-tmpl-js',
-					'src'  => PLUGIN_URL . 'extra/bri_tax_extra/assets/js/portfolio.js',
-					'deps' => [ 'jquery', 'imagesloaded', 'bri-bootstrap-js', 'bri-magnific-popup-js', 'bri-isotop-js' ],
-					'ver'  => '1.0.0',
-					'in_footer' => true
-				]
-			]
-		];
-
+		require_once( PLUGIN_PATH . 'extra/bri_tax_extra/assets_paths.php' );
 		$assets = apply_filters( "bri_tax_tmpls_assets", $assets );
 
 		// Helper::debug( $assets );
