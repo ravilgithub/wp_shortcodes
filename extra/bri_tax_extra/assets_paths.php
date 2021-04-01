bb1141ce3fae1c9f2170468d909bb3870fff7c8c
ravilgithub <foragetonly800@mail.ru>
2021-04-01 23:34:44 +0300

    Регистрация стилей и скриптов шаблонов 'features' и 'stickers'

--- extra/bri_tax_extra/assets_paths.php
+++ extra/bri_tax_extra/assets_paths.php
@@ -14,6 +14,18 @@ $assets = [
 			'deps' => [ 'bri-bootstrap-css', 'bri-fontawesome-css' ],
 			'ver'  => '1.0.0'
 		],
+		'features' => [
+			'id'   => 'features-tmpl-css',
+			'src'  => PLUGIN_URL . 'extra/bri_tax_extra/assets/css/features.min.css',
+			'deps' => [ 'bri-bootstrap-css', 'bri-fontawesome-css' ],
+			'ver'  => '1.0.0'
+		],
+		'stickers' => [
+			'id'   => 'stickers-tmpl-css',
+			'src'  => PLUGIN_URL . 'extra/bri_tax_extra/assets/css/stickers.min.css',
+			'deps' => [ 'bri-bootstrap-css' ],
+			'ver'  => '1.0.0'
+		],
 	],
 
 	'js' => [
@@ -22,6 +34,20 @@ $assets = [
 			'id'        => 'portfolio-tmpl-js',
 			'src'       => PLUGIN_URL . 'extra/bri_tax_extra/assets/js/portfolio.js',
 			'deps'      => [ 'jquery', 'imagesloaded', 'bri-bootstrap-js', 'bri-magnific-popup-js', 'bri-isotop-js' ],
+			'ver'       => '1.0.0',
+			'in_footer' => true
+		],
+		'features' => [
+			'id'        => 'features-tmpl-js',
+			'src'       => PLUGIN_URL . 'extra/bri_tax_extra/assets/js/features.js',
+			'deps'      => [ 'jquery', 'bri-bootstrap-js', 'bri-imagesloaded-js' ],
+			'ver'       => '1.0.0',
+			'in_footer' => true
+		],
+		'stickers' => [
+			'id'        => 'stickers-tmpl-js',
+			'src'       => PLUGIN_URL . 'extra/bri_tax_extra/assets/js/stickers.js',
+			'deps'      => [ 'jquery', 'bri-bootstrap-js', 'bri-masonry-js' ],
 			'ver'       => '1.0.0',
 			'in_footer' => true
 		]
