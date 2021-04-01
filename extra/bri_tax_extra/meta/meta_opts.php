320f6f1be14f62ec0792cd7da492dbc60a10e861
ravilgithub <foragetonly800@mail.ru>
2021-04-01 23:52:44 +0300

    Add - Добавил параметы мета боксов для шаблона 'features'

--- extra/bri_tax_extra/meta/meta_opts.php
+++ extra/bri_tax_extra/meta/meta_opts.php
@@ -183,6 +183,22 @@ $opts = [
 				],
 			],
 		],
+		'features' => [
+			'fields' => [
+				'icon' => [
+					'type'  => 'text',
+					'title' => 'Имя иконки',
+					'desc'  => 'Часть класса иконки "Font Awesome".<br /> Пример: fa fa-<имя иконки>',
+					'value' => ''
+				],
+				'label' => [
+					'type'  => 'text',
+					'title' => 'Лейбл',
+					'desc'  => 'Название табуляции',
+					'value' => ''
+				],
+			],
+		],
 		'about' => [
 			'fields' => [
 				'option_1' => [
