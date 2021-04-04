4e597f67369604f93319ea63cd33b0495e2c9dc0
ravilgithub <foragetonly800@mail.ru>
2021-04-05 01:14:48 +0300

    Change - Для каждого шаблона будет создаваться свой объект 'stkr'. Add - Добавил 'ImageLoaded'. Documentation

--- extra/bri_tax_extra/assets/js/stickers.js
+++ extra/bri_tax_extra/assets/js/stickers.js
@@ -3,48 +3,164 @@
 
 	$( document ).ready( () => {
 
-		// Stickers Massonry
+		/**
+		 * Stickers Actions.
+		 *
+		 * @property {String} ctx  - селектор каждого шаблона "stickers".
+		 * @property {Object} stkr - объект создающийся для каждого шаблона "stickers".
+		 *
+		 * @since 0.0.1
+		 * @author Ravil.
+		 */
 		const $stickersMassonry = {
-			msnry: false,
-			ctx: '.stickers-content-grid',
+			ctx: '.bri-stickers-tmpl',
 
-			msnryInit () {
-				if ( ! this.msnry ) {
-					this.msnry = $( this.ctx ).masonry( {
-						itemSelector: '.stickers-content-item',
-						columnWidth: '.stickers-content-item',
-						percentPosition: true,
+			/**
+			 * Объект создающийся для каждого шаблона "stickers".
+			 *
+			 * @property {Boolean / Object} msnry - объект Masonry. Default: false.
+			 * @property {Object} dom - jQuery объекты элементов шаблона "stickers".
+			 *
+			 * @since 0.0.1
+			 */
+			stkr: {
+				msnry: false,
+				dom: {},
+
+
+				/**
+				 * Инициализация объекта "Masonry".
+				 *
+				 * @return {void}
+				 *
+				 * @since 0.0.1
+				 */
+				msnryInit () {
+					if ( ! this.msnry ) {
+						this.dom.grid.imagesLoaded()
+							.done( instance => {
+								this.msnry = this.dom.grid.masonry( {
+									itemSelector: this.dom.item,
+									columnWidth: this.dom.item,
+									percentPosition: true,
+								} );
+							} )
+							.fail( err => {
+								console.log( err );
+							} );
+					}
+				},
+
+
+				/**
+				 * Пересборка элементов.
+				 *
+				 * @return {void}
+				 *
+				 * @since 0.0.1
+				 */
+				msnryReload() {
+					if ( this.msnry ) {
+						this.msnry.masonry( 'reloadItems' );
+					}
+				},
+
+
+				/**
+				 * Удаление всего функционала "Masonry".
+				 *
+				 * @return {void}
+				 *
+				 * @since 0.0.1
+				 */
+				msnryDestroy() {
+					if ( this.msnry ) {
+						this.msnry.masonry( 'destroy' );
+						this.msnry = false;
+					}
+				},
+
+
+				/**
+				 * Определение действий в зависимости от ширины экрана.
+				 *
+				 * @return {void}
+				 *
+				 * @since 0.0.1
+				 */
+				setMasonry() {
+					// Потестить на Android
+					// для FireFox
+					if ( $( window ).width() > 976 ) {
+					// Для Opera
+					// if ( ( ( ! Modernizr.touch ) && ( $( window ).width() > 976 ) ) || ( $( window ).width() > 991 ) ) {
+						this.msnryInit();
+						this.msnryReload();
+					} else {
+						this.msnryDestroy();
+					}
+				},
+
+
+				/**
+				 * Обработчик события "resize".
+				 *
+				 * @return {void}
+				 *
+				 * @since 0.0.1
+				 */
+				resizeHandler() {
+					$( window ).on( 'resize', () => {
+						this.setMasonry();
 					} );
+				},
+
+
+				/**
+				 * Добавление jQuery объекты элементов шаблона "stickers"
+				 * в свойство "this.dom" для кратости кода.
+				 *
+				 * @return {void}
+				 *
+				 * @since 0.0.1
+				 */
+				setProps() {
+					let dom = this.dom;
+					dom[ 'grid' ] = dom.tmpl.find( '.stickers-content-grid' );
+					dom[ 'item' ] = '.stickers-content-item';
+				},
+
+
+				/**
+				 * Let's go.
+				 *
+				 * @return {void}
+				 *
+				 * @since 0.0.1
+				 */
+				start() {
+					this.setProps();
+					this.setMasonry();
 				}
 			},
 
-			msnryReload() {
-				if ( this.msnry ) {
-					this.msnry.masonry( 'reloadItems' );
-				}
-			},
 
-			msnryDestroy() {
-				if ( this.msnry ) {
-					this.msnry.masonry( 'destroy' );
-					this.msnry = false;
-				}
-			},
-
-			setMasonry() {
-				// Потестить на Android
-				if ( $( window ).width() > 976 ) {  // для FireFox
-				// if ( $( window ).width() > 991 ) {  // для FireFox
-				// if ( ( ( ! Modernizr.touch ) && ( $( window ).width() > 976 ) ) || ( $( window ).width() > 991 ) ) {  // Для Opera
-					this.msnryInit();
-					this.msnryReload();
-				} else {
-					this.msnryDestroy();
-				}
-			},
-
+			/**
+			 * Создание независимого объекта "stkr"
+			 * для каждого шаблона "stickers".
+			 *
+			 * @return {void}
+			 *
+			 * @since 0.0.1
+			 */
 			init() {
-				this.setMasonry();
+				$( this.ctx ).each( ( idx, $el ) => {
+					let newStkr = { dom: {} };
+					newStkr.dom[ 'tmpl' ] = $( $el );
+
+					$.extend( true, newStkr, this.stkr );
+					newStkr.start();
+				} );
 			},
 		};
 
