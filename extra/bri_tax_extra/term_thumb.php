343e9bb12973cb25b6c6bb70497b47ef8d4bf0cf
ravilgithub <foragetonly800@mail.ru>
2021-04-12 03:21:28 +0300

    Add - Подключил файлы JS, CSS. Добавил разметку метаполя на странице создания термина

--- extra/bri_tax_extra/term_thumb.php
+++ extra/bri_tax_extra/term_thumb.php
@@ -2,10 +2,45 @@
 namespace Bri_Shortcodes;
 
 class Term_img {
-	public $holder = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkAQMAAABKLAcXAAAABlBMVEUAAAC7u7s37rVJAAAAAXRSTlMAQObYZgAAACJJREFUOMtjGAV0BvL/G0YMr/4/CDwY0rzBFJ704o0CWgMAvyaRh+c6m54AAAAASUVORK5CYII=';
+	public $holder = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAABSUlEQVRoge2aSw6DMAxEe/+zecsFkOAQbKYLZAQ0HyexS+J2JKSoBHteBcEkeRERlmUZ+iAivLgxqogI67ruIPzDaGLPF5DziRF09voBcu/Qq+4egyChjj0p5C0KErvgacU8JUFSFz6hlJcsSC7At5TzIAKRBLKUJLcYRBpQW9KcRSAlgTVUkqsYpDRBrUpzVIHUJLKOXQ1Sm9AqZhNIS2LtWM0grQa0YqiAtBrR+CPUQIA6Q1q3pioIUGZM8/lSBwFkBrVHPBMQIG3UYtg2AwHChq1epKYgwNW4ZTVgDgLsANb12R9EKhe3louH3cXw6+KF6KJEcVE0uijjXXxYufjUdTH54GI6yMUEnYspUxeT2E8uK0zTlO3zOws9PUCw5nmOnhtuMTQG43t5umcIVnbDwAgQrG3bjra/TTUjQrCObU7cGPkgIrwB11J3MEekPv4AAAAASUVORK5CYII=';
 
 	public function __construct( Array $taxs ) {
+		$this->add_assets();
 		$this->add_hooks( $taxs );
+	}
+
+	public function add_assets() {
+		$assets = [
+			'css' => [
+				/************ TMPL CSS ************/
+				'id'   => 'term-img-css',
+				'src'  => PLUGIN_URL . 'extra/bri_tax_extra/assets/css/term_img.min.css',
+				'deps' => [],
+				'ver'  => '1.0.0'
+			],
+			'js' => [
+				/************ TMPL SCRIPTS ************/
+				'id'   => 'term-img-js',
+				'src'  => PLUGIN_URL . 'extra/bri_tax_extra/assets/js/term_img.js',
+				'deps' => [ 'jquery' ],
+				'ver'  => '1.0.0',
+				'in_footer' => true
+			]
+		];
+
+		$assets = apply_filters( 'BRIZ_Term_img_assets', $assets );
+
+		foreach ( $assets as $type => $data ) {
+			extract( $data );
+
+			if ( 'css' == $type ) {
+				if ( ! wp_style_is( $id, 'enqueued' ) )
+					wp_enqueue_style( $id, $src, $deps, $ver );
+			} else {
+				if ( ! wp_script_is( $id, 'enqueued' ) )
+					wp_enqueue_script( $id, $src, $deps, $ver, $in_footer );
+			}
+		}
 	}
 
 	public function add_hooks( $taxs ) {
@@ -43,7 +78,24 @@ class Term_img {
 	}
 
 	public function add_term_img ( $tax_slug ) {
-		
+?>
+		<div id="briz-term-img-wrap" class="form-field">
+			<input type="hidden" name="briz-term-img-id" value="">
+
+			<label><?php _e( 'Image' ); ?></label>
+
+			<figure>
+				<a href="#">
+					<img src="<?php echo esc_attr( $this->holder ); ?>" alt="Alt" />
+				</a>
+				<button type="button" class="button">
+					<?php _e( 'Remove' ); ?>
+				</button>
+			</figure>
+
+			<p><?php _e( 'Description' ); ?></p>
+		</div>
+<?php
 	}
 
 	public function edit_term_img ( $term ) {
@@ -55,11 +107,11 @@ class Term_img {
 	}
 
 	public function add_img_column ( $cols ) {
-		
+		return $cols;
 	}
 
 	public function fill_img_column ( $str, $col_name, $term_id ) {
-		
+		return $str;
 	}
 }
 
@@ -70,7 +122,10 @@ function term_img_init() {
 		'product_cat'
 	];
 
-	$taxs = add_filter( 'BRI_Term_img_atts', $taxs );
+	$taxs = apply_filters( 'BRI_Term_img_atts', $taxs );
+
+	/*Helper::debug( $taxs );
+	exit;*/
 
 	if ( ! empty( $taxs ) )
 		new Term_img( $taxs );
