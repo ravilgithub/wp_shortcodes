2352898bf14b5ae0e02a5887fcd527ea4fef8d1b
ravilgithub <foragetonly800@mail.ru>
2021-04-12 03:26:40 +0300

    Add - Добавил файл JS метаполя 'миниатюра термина'

--- extra/bri_tax_extra/assets/js/term_img.js
+++ extra/bri_tax_extra/assets/js/term_img.js
@@ -3,7 +3,29 @@
 
 	$( document ).ready( () => {
 
-		console.log( 'term_img' );
+		const media = {
+			add() {
+				$( '#briz-term-img-wrap a' ).on( 'click', evt => {
+					evt.preventDefault();
+
+					const args = {
+						title: 'Select image',
+						library: { type: 'image' },
+						multiple: 0,
+						button: { text: 'Insert' }
+					},
+					wpm = wp.media( args );
+
+					wpm.open();
+				} );
+			},
+
+			init() {
+				this.add();
+			}
+		};
+
+		media.init();
 
 	} );
 
