59605131150aa44eab54884f1738cb1c18888fd0
ravilgithub <foragetonly800@mail.ru>
2021-04-24 16:53:13 +0300

    Documentation

--- extra/bri_tax_extra/assets/js/term_img.js
+++ extra/bri_tax_extra/assets/js/term_img.js
@@ -3,6 +3,17 @@
 
 	$( document ).ready( () => {
 
+		/**
+		 * Функционал описывающий работу с медиа файлами.
+		 * Добавление, изменение и удаление медиа файлов в мета полях.
+		 *
+		 * @property String ctx     - селектор обёртки мета поля.
+		 * @property Object wpm     - WP Media Object.
+		 * @property Object wpmArgs - параметры передаваемые в WP Media Object.
+		 *
+		 * @since 0.0.1
+		 * @autor Ravil
+		 */
 		const media = {
 			ctx: '#briz-term-img-wrap',
 			wpm: null,
@@ -13,6 +24,13 @@
 				button: { text: 'Insert' }
 			},
 
+
+			/**
+			 * Создаём объект wp.media и
+			 * передаём ему первоначальные данные.
+			 *
+			 * @return {void}
+			 */
 			add() {
 				$( 'a', this.ctx ).on( 'click', evt => {
 					evt.preventDefault();
@@ -26,6 +44,13 @@
 				} );
 			},
 
+
+			/**
+			 * Обработчик открыия медиа библиотеки.
+			 * Помечаем раннее выбранные медиа файлы если они есть.
+			 *
+			 * @return {void}
+			 */
 			open() {
 				this.wpm.on( 'open', () => {
 					const id  = $( 'input', this.ctx ).val();
@@ -40,6 +65,12 @@
 				} );
 			},
 
+
+			/**
+			 * Обработчик выбора медиа данных в библиотеке.
+			 *
+			 * @return {void}
+			 */
 			select() {
 				this.wpm.on( 'select', () => {
 					const img = this.wpm.state().get( 'selection' ).first().toJSON();
@@ -52,6 +83,15 @@
 				} );
 			},
 
+
+			/**
+			 * Присваиваем атрибуты HTML элементам.
+			 *
+			 * @param Integer id - id медиа файла.
+			 * @param String url - url медиа файла.
+			 *
+			 * @return {void}
+			 */
 			setAtts( id = '', url = '' ) {
 				if ( ! url )
 					url = $( 'img', this.ctx ).data( 'default' );
@@ -60,6 +100,14 @@
 				$( 'img', this.ctx ).attr( 'src', url );
 			},
 
+
+			/**
+			 * Прячем или показываем кнопку удаления из мета поля медиа файла.
+			 *
+			 * @param String action - действие которое совершается.
+			 *
+			 * @return {void}
+			 */
 			btn( action ) {
 				const btn = $( 'button', this.ctx );
 
@@ -70,6 +118,12 @@
 				}
 			},
 
+
+			/**
+			 * Удаление медиа данных.
+			 *
+			 * @return {void}
+			 */
 			del() {
 				$( 'button', this.ctx ).on( 'click', () => {
 					this.setAtts();
@@ -77,6 +131,12 @@
 				} );
 			},
 
+
+			/**
+			 * Инициализация основных методов.
+			 *
+			 * @return {void}
+			 */
 			init() {
 				this.add();
 				this.del();
