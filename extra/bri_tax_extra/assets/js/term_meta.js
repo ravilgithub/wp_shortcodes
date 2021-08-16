ddad00fbe8977040138372e4ee31e8446d028429
Ravil <foragetonly800@mail.ru>
2021-08-16 20:15:46 +0300

    DELETED

--- extra/bri_tax_extra/assets/js/term_meta.js
+++
@@ -1,8 +0,0 @@
-import range from './export/range.js';
-range.init( '.term-briz-range-wrap' );
-
-import media from './export/media.js';
-media.init( '.briz-term-img-wrap');
-
-import media_button from './export/media_button.js';
-media_button.init( '.briz-term-media-btn-wrap' );
