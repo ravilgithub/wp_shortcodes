4d065b70b985366db693dd3652f2f03453859eee
Ravil <foragetonly800@mail.ru>
2021-08-16 20:14:05 +0300

    DELETED

--- extra/bri_tax_extra/assets/js/metabox.js
+++
@@ -1,5 +0,0 @@
-import range from './export/range.js';
-range.init( '.briz-meta-field-range-wrap' );
-
-import media_button from './export/media_button.js';
-media_button.init( '.briz-media-button' );
