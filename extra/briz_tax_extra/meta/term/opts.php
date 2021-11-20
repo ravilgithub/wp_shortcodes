8299ddcd8944a7d175de437dc19ebb9a61af06bc
ravilgithub <foragetonly800@mail.ru>
2021-11-21 02:09:53 +0300

    Add - Добавил мета поля для термина 'Stickers'

--- extra/briz_tax_extra/meta/term/opts.php
+++ extra/briz_tax_extra/meta/term/opts.php
@@ -162,6 +162,36 @@ $opts = [
 			]
 		],
 
+		'stickers' => [
+			'fields' => [
+				'bg_img' => [
+					'type'  => 'media_button',
+					'title' => 'Background image',
+					'desc'  => '',
+					'value' => '[859]',
+					'empty' => false,
+					'options' => [
+						'title'    => 'Insert a media',
+						'library'  => [ 'type' => 'image' ],
+						'multiple' => true,
+						'button'   => [ 'text' => 'Insert' ]
+					],
+				],
+				'bg_attachment' => [
+					'type'  => 'select',
+					'title' => 'Background attachment',
+					'desc'  => '',
+					'value' => 'fixed',
+					'options' => [
+						'default'  => 'default',
+						'fixed'    => 'Fixed',
+						'parallax' => 'Parallax',
+						'hidden'   => 'Hidden'
+					]
+				],
+			]
+		],
+
 
 		'option_12' => [
 			'type'  => 'media_button',
