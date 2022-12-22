89544b87563c0e8d9496ea9b4327cf0eb2618251
Ravil <foragetonly800@mail.ru>
2022-12-22 09:06:03 +0300

    Add/Change - Добавил мета поля для заголовка и контента шаблона 'Brands'. Изменил параметры по умолчанию для слайдера в шаблоне 'Brands'.

--- extra/briz_tax_extra/meta/term/exports/category/brands_meta_fields.php
+++ extra/briz_tax_extra/meta/term/exports/category/brands_meta_fields.php
@@ -4,19 +4,123 @@
  */
 $brands_meta_fields = [
 	'fields' => [
-		'section_header_first' => [
+		'header' => [
+			'type'  => 'checkbox',
+			'title' => 'Заголовок',
+			'desc'  => 'Показать/Спрятать заголовок.',
+			'empty' => true,
+			'value' => [ 'on' ],
+			'options' => [
+				'on' => 'On'
+			]
+		],
+		'header_first' => [
 			'type'  => 'text',
 			'title' => 'Заголовок секции первая часть',
 			'desc'  => 'Первая часть заголовка секции.',
 			'empty' => true,
-			'value' => ''
+			'value' => 'Brand'
 		],
-		'section_header_last' => [
+		'header_last' => [
 			'type'  => 'text',
 			'title' => 'Заголовок секции вторая часть',
 			'desc'  => 'Вторая часть заголовка секции.',
 			'empty' => true,
-			'value' => ''
+			'value' => '\'s'
+		],
+		'header_spacer' => [
+			'type'  => 'checkbox',
+			'title' => 'Header spacer',
+			'desc'  => 'Show/Hide header spacer.',
+			'empty' => true,
+			'value' => '',
+			'options' => [
+				'on' => 'On'
+			]
+		],
+		'header_description' => [
+			'type'  => 'checkbox',
+			'title' => 'Описание',
+			'desc'  => 'Показать/Спрятать описание.',
+			'empty' => true,
+			'value' => '',
+			'options' => [
+				'on' => 'On'
+			]
+		],
+		'header_description_text' => [
+			'type'  => 'textarea',
+			'title' => 'Текст описания',
+			'desc'  => 'Текст описания секции',
+			'empty' => true,
+			'value' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt deserunt iste, veniam necessitatibus repellat quis.'
+		],
+		'bg_img' => [
+			'type'  => 'media_button',
+			'title' => 'Background image',
+			'desc'  => '',
+			'value' => '[857]',
+			'empty' => false,
+			'options' => [
+				'title'    => 'Insert a media',
+				'library'  => [ 'type' => 'image' ],
+				'multiple' => false,
+				'button'   => [ 'text' => 'Insert' ]
+			],
+		],
+		'bg_attachment' => [
+			'type'  => 'select',
+			'title' => 'Background attachment',
+			'desc'  => '',
+			'value' => 'default',
+			'options' => [
+				'default'  => 'default',
+				'fixed'    => 'Fixed',
+				'parallax' => 'Parallax',
+				'hidden'   => 'Hidden'
+			]
+		],
+		'header_bg_color_enable' => [
+			'type'  => 'checkbox',
+			'title' => 'Показать цвет фона заголовка',
+			'desc'  => 'Показать цвет фона заголовка.',
+			'empty' => true,
+			'value' => '',
+			'options' => [
+				'on' => 'On'
+			]
+		],
+		'header_bg_color' => [
+			'type'  => 'color',
+			'title' => 'Цвет фона заголовка',
+			'desc'  => 'Цвет фона заголовка',
+			'value' => '#d9ffd1'
+		],
+		'content_bg_color_enable' => [
+			'type'  => 'checkbox',
+			'title' => 'Показать цвет фона контента',
+			'desc'  => 'Показать цвет фона контента.',
+			'empty' => true,
+			'value' => '',
+			'options' => [
+				'on' => 'On'
+			]
+		],
+		'content_bg_color' => [
+			'type'  => 'color',
+			'title' => 'Цвет фона контента',
+			'desc'  => 'Цвет фона контента',
+			'value' => '#b0c4de'
+		],
+		'content_wide' => [
+			'type'  => 'checkbox',
+			'title' => 'Широкий контент',
+			'desc'  => 'Контент на всю ширину экрана.',
+			'empty' => true,
+			'value' => '',
+			'options' => [
+				'on' => 'On'
+			]
 		],
 		'slider_params' => [
 			'type'  => 'group',
@@ -39,7 +143,7 @@ $brands_meta_fields = [
 					'desc'  => 'Описание опции 11_2',
 					'value' => 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADwAAAA8CAYAAAA6/NlyAAABjElEQVRoge2bWQ6DMAxEe/+z+RAICQ7Bz/SjStWFkM02IcNISFEJ9rwKhawPEcGyLBSXiOARCqNLRLCu6ws4/DCqAtsX8OeNkfTJ9Af8W+Hq+mXZBd6reEXtMUSBYw9cRTHvh8BHD/asI89J4FSA3pTymgWcE6gH5XjMBs4NeJZyvRUBlwT2VImnYuDSBNYq9VIFXJPIQjUeqoFrE2qpNncTcEviFrXkbAZuNeCdSwVYw4hXDjVgwBZaK7YqMGADrRlTHRjQNaj9B5oAAzpGLd4WM2CgzbBVe2AKDNQZt2z8zIGBMgDrz5sLMJAH4vEtdwMGjoG8emuuwMA+mGfX1B0Y+Ab0HnycAgy8QM8Yad3AHqJ6pakaLarPElXHg6prSTV4oBoeUk0AUE3xUE3iUU3TUk3EUy21UC2mUS2XUi2I9wAbZL7loSfYILNNLT3CBqlvW+oZNmiapmSde+thaYAeNc9z9N6w24dj0PcG8VjFqyp5BGAk2KBt295l3mM8I8MGvQ9qhQLDJSJ4AsaVfzhlR3kRAAAAAElFTkSuQmCC'
 				],*/
-				'icon' => [
+				/*'icon' => [
 					'type'  => 'text',
 					'title' => 'Имя иконки',
 					'desc'  => 'Часть класса иконки "Font Awesome".<br /> Пример: fa fa-<имя иконки>',
@@ -111,7 +215,7 @@ $brands_meta_fields = [
 						'green' => 'Green',
 						'blue'  => 'Blue'
 					]
-				],
+				],*/
 
 				/* END для тестов */
 
@@ -119,7 +223,7 @@ $brands_meta_fields = [
 					'type'  => 'number',
 					'title' => 'Количество слайдов',
 					'desc'  => 'Количество видимых слайдов.',
-					'value' => 6,
+					'value' => 1,
 					'options' => [
 						'step' => 1,
 						'min'  => 1,
@@ -152,13 +256,44 @@ $brands_meta_fields = [
 					]
 				],
 
+				/*'autoplay' => [
+					'type'  => 'group',
+					'title' => 'Autoplay',
+					'desc'  => 'Авто прокрутка',
+					'color' => '#8fa',
+					'value' => [
+						'delay' => [
+							'type'  => 'number',
+							'title' => 'Задержка',
+							'desc'  => 'Количество милисекунд до начала автопрокрутки. 0 - выкл.',
+							'value' => '5000',
+							'options' => [
+								'step' => 10,
+								'min'  => '0',
+								'max'  => ''
+							]
+						],
+					]
+				],*/
+
 				'autoplay' => [
-					'type'  => 'number',
-					'title' => 'Автопрокрутка',
-					'desc'  => 'Количество милисекунд до начала автопрокрутки. 0 - выкл.',
-					'value' => 5000,
+					'type' => 'checkbox',
+					'title' => 'Autoplay',
+					'desc'  => 'Авто прокрутка',
+					'empty' => true,
+					'value' => [],
 					'options' => [
-						'step' => 1,
+						'on'  => 'On'
+					]
+				],
+
+				'autoplay_delay' => [
+					'type'  => 'number',
+					'title' => 'Задержка',
+					'desc'  => 'Количество милисекунд до начала автопрокрутки. default( 2000 )',
+					'value' => '5000',
+					'options' => [
+						'step' => 10,
 						'min'  => '0',
 						'max'  => ''
 					]
@@ -224,7 +359,7 @@ $brands_meta_fields = [
 									'type'  => 'number',
 									'title' => 'Slider per view',
 									'desc'  => 'Количество видимых слайдов при разрешении 320px',
-									'value' => 1,
+									'value' => 2,
 									'empty' => true,
 									'options' => [
 										'step' => 1,
@@ -257,7 +392,7 @@ $brands_meta_fields = [
 									'type'  => 'number',
 									'title' => 'Slider per view',
 									'desc'  => 'Количество видимых слайдов при разрешении 480px',
-									'value' => 2,
+									'value' => 3,
 									'empty' => true,
 									'options' => [
 										'step' => 1,
@@ -280,17 +415,17 @@ $brands_meta_fields = [
 							]
 						],
 
-						'567' => [
+						'568' => [
 							'type'  => 'group',
-							'title' => '567',
-							'desc'  => 'Параметры 567',
+							'title' => '568',
+							'desc'  => 'Параметры 568',
 							// 'color' => '#0ff',
 							'value' => [
 								'slidesPerView' => [
 									'type'  => 'number',
 									'title' => 'Slider per view',
-									'desc'  => 'Количество видимых слайдов при разрешении 567px',
-									'value' => 3,
+									'desc'  => 'Количество видимых слайдов при разрешении 568px',
+									'value' => 4,
 									'empty' => true,
 									'options' => [
 										'step' => 1,
@@ -301,7 +436,7 @@ $brands_meta_fields = [
 								'spaceBetween' => [
 									'type'  => 'number',
 									'title' => 'Space between',
-									'desc'  => 'Промежуток между слайдерами в px при разрешении 567px',
+									'desc'  => 'Промежуток между слайдерами в px при разрешении 568px',
 									'value' => 30,
 									'empty' => true,
 									'options' => [
@@ -379,17 +514,17 @@ $brands_meta_fields = [
 							]
 						],
 
-						'991' => [
+						'992' => [
 							'type'  => 'group',
-							'title' => '991',
-							'desc'  => 'Параметры 991',
+							'title' => '992',
+							'desc'  => 'Параметры 992',
 							// 'color' => '#0ff',
 							'value' => [
 								'slidesPerView' => [
 									'type'  => 'number',
 									'title' => 'Slider per view',
-									'desc'  => 'Количество видимых слайдов при разрешении 991px',
-									'value' => 4,
+									'desc'  => 'Количество видимых слайдов при разрешении 992px',
+									'value' => 6,
 									'empty' => true,
 									'options' => [
 										'step' => 1,
@@ -400,7 +535,40 @@ $brands_meta_fields = [
 								'spaceBetween' => [
 									'type'  => 'number',
 									'title' => 'Space between',
-									'desc'  => 'Промежуток между слайдерами в px при разрешении 991px',
+									'desc'  => 'Промежуток между слайдерами в px при разрешении 992px',
+									'value' => 30,
+									'empty' => true,
+									'options' => [
+										'step' => 1,
+										'min'  => '0',
+										'max'  => ''
+									]
+								],
+							]
+						],
+
+						'1200' => [
+							'type'  => 'group',
+							'title' => '1200',
+							'desc'  => 'Параметры 1200',
+							// 'color' => '#0ff',
+							'value' => [
+								'slidesPerView' => [
+									'type'  => 'number',
+									'title' => 'Slider per view',
+									'desc'  => 'Количество видимых слайдов при разрешении 1200px',
+									'value' => 6,
+									'empty' => true,
+									'options' => [
+										'step' => 1,
+										'min'  => 1,
+										'max'  => ''
+									]
+								],
+								'spaceBetween' => [
+									'type'  => 'number',
+									'title' => 'Space between',
+									'desc'  => 'Промежуток между слайдерами в px при разрешении 1200px',
 									'value' => 30,
 									'empty' => true,
 									'options' => [
