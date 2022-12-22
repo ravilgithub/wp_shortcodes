a54b3394d5e8f4f247e38a436453379daea7b29a
Ravil <foragetonly800@mail.ru>
2022-12-22 09:15:19 +0300

    Add/Change - Добавил мета поля для заголовка и контента шаблона 'Review'. Изменил параметры по умолчанию для слайдера в шаблоне 'Review'.

--- extra/briz_tax_extra/meta/term/exports/category/review_meta_fields.php
+++ extra/briz_tax_extra/meta/term/exports/category/review_meta_fields.php
@@ -4,19 +4,123 @@
  */
 $review_meta_fields = [
 	'fields' => [
-		'section_header_first' => [
+		'header' => [
+			'type'  => 'checkbox',
+			'title' => 'Заголовок',
+			'desc'  => 'Показать/Спрятать заголовок.',
+			'empty' => true,
+			'value' => [ 'on' ],
+			'options' => [
+				'on' => 'On'
+			]
+		],
+		'header_first' => [
 			'type'  => 'text',
 			'title' => 'Заголовок секции первая часть',
 			'desc'  => 'Первая часть заголовка секции.',
 			'empty' => true,
-			'value' => ''
+			'value' => 'Clients'
 		],
-		'section_header_last' => [
+		'header_last' => [
 			'type'  => 'text',
 			'title' => 'Заголовок секции вторая часть',
 			'desc'  => 'Вторая часть заголовка секции.',
 			'empty' => true,
-			'value' => ''
+			'value' => 'review'
+		],
+		'header_spacer' => [
+			'type'  => 'checkbox',
+			'title' => 'Header spacer',
+			'desc'  => 'Show/Hide header spacer.',
+			'empty' => true,
+			'value' => '',
+			'options' => [
+				'on' => 'On'
+			]
+		],
+		'header_description' => [
+			'type'  => 'checkbox',
+			'title' => 'Описание',
+			'desc'  => 'Показать/Спрятать описание.',
+			'empty' => true,
+			'value' => '',
+			'options' => [
+				'on' => 'On'
+			]
+		],
+		'header_description_text' => [
+			'type'  => 'textarea',
+			'title' => 'Текст описания',
+			'desc'  => 'Текст описания секции',
+			'empty' => true,
+			'value' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt deserunt iste, veniam necessitatibus repellat quis.'
+		],
+		'bg_img' => [
+			'type'  => 'media_button',
+			'title' => 'Background image',
+			'desc'  => '',
+			'value' => '[802]',
+			'empty' => false,
+			'options' => [
+				'title'    => 'Insert a media',
+				'library'  => [ 'type' => 'image' ],
+				'multiple' => true,
+				'button'   => [ 'text' => 'Insert' ]
+			],
+		],
+		'bg_attachment' => [
+			'type'  => 'select',
+			'title' => 'Background attachment',
+			'desc'  => '',
+			'value' => 'default',
+			'options' => [
+				'default'  => 'default',
+				'fixed'    => 'Fixed',
+				'parallax' => 'Parallax',
+				'hidden'   => 'Hidden'
+			]
+		],
+		'header_bg_color_enable' => [
+			'type'  => 'checkbox',
+			'title' => 'Показать цвет фона заголовка',
+			'desc'  => 'Показать цвет фона заголовка.',
+			'empty' => true,
+			'value' => '',
+			'options' => [
+				'on' => 'On'
+			]
+		],
+		'header_bg_color' => [
+			'type'  => 'color',
+			'title' => 'Цвет фона заголовка',
+			'desc'  => 'Цвет фона заголовка',
+			'value' => '#d9ffd1'
+		],
+		'content_bg_color_enable' => [
+			'type'  => 'checkbox',
+			'title' => 'Показать цвет фона контента',
+			'desc'  => 'Показать цвет фона контента.',
+			'empty' => true,
+			'value' => '',
+			'options' => [
+				'on' => 'On'
+			]
+		],
+		'content_bg_color' => [
+			'type'  => 'color',
+			'title' => 'Цвет фона контента',
+			'desc'  => 'Цвет фона контента',
+			'value' => '#b0c4de'
+		],
+		'content_wide' => [
+			'type'  => 'checkbox',
+			'title' => 'Широкий контент',
+			'desc'  => 'Контент на всю ширину экрана.',
+			'empty' => true,
+			'value' => '',
+			'options' => [
+				'on' => 'On'
+			]
 		],
 		'slider_params' => [
 			'type'  => 'group',
@@ -28,7 +132,7 @@ $review_meta_fields = [
 					'type'  => 'number',
 					'title' => 'Количество слайдов',
 					'desc'  => 'Количество видимых слайдов.',
-					'value' => 2,
+					'value' => 1,
 					'options' => [
 						'step' => 1,
 						'min'  => 1,
@@ -62,12 +166,23 @@ $review_meta_fields = [
 				],
 
 				'autoplay' => [
-					'type'  => 'number',
-					'title' => 'Автопрокрутка',
-					'desc'  => 'Количество милисекунд до начала автопрокрутки. 0 - выкл.',
-					'value' => '0',
+					'type' => 'checkbox',
+					'title' => 'Autoplay',
+					'desc'  => 'Авто прокрутка',
+					'empty' => true,
+					'value' => [],
 					'options' => [
-						'step' => 1,
+						'on'  => 'On'
+					]
+				],
+
+				'autoplay_delay' => [
+					'type'  => 'number',
+					'title' => 'Задержка',
+					'desc'  => 'Количество милисекунд до начала автопрокрутки. default( 2000 )',
+					'value' => '5000',
+					'options' => [
+						'step' => 10,
 						'min'  => '0',
 						'max'  => ''
 					]
@@ -83,6 +198,26 @@ $review_meta_fields = [
 						'on'  => 'On'
 					]
 				],
+
+				// 'autoplay' => [
+				// 	'type'  => 'group',
+				// 	'title' => 'Autoplay',
+				// 	'desc'  => 'Авто прокрутка',
+				// 	'color' => '#8fa',
+				// 	'value' => [
+				// 		'delay' => [
+				// 			'type'  => 'number',
+				// 			'title' => 'Задержка',
+				// 			'desc'  => 'Количество милисекунд до начала автопрокрутки. 0 - выкл.',
+				// 			'value' => '2000',
+				// 			'options' => [
+				// 				'step' => 10,
+				// 				'min'  => '0',
+				// 				'max'  => ''
+				// 			]
+				// 		],
+				// 	]
+				// ],
 
 				'breakpoints' => [
 					'type'  => 'group',
@@ -189,16 +324,16 @@ $review_meta_fields = [
 							]
 						],
 
-						'567' => [
+						'568' => [ // ?? 567
 							'type'  => 'group',
-							'title' => '567',
-							'desc'  => 'Параметры 567',
+							'title' => '568',
+							'desc'  => 'Параметры 568',
 							// 'color' => '#0ff',
 							'value' => [
 								'slidesPerView' => [
 									'type'  => 'number',
 									'title' => 'Slider per view',
-									'desc'  => 'Количество видимых слайдов при разрешении 567px',
+									'desc'  => 'Количество видимых слайдов при разрешении 568px',
 									'value' => 1,
 									'empty' => true,
 									'options' => [
@@ -210,7 +345,7 @@ $review_meta_fields = [
 								'spaceBetween' => [
 									'type'  => 'number',
 									'title' => 'Space between',
-									'desc'  => 'Промежуток между слайдерами в px при разрешении 567px',
+									'desc'  => 'Промежуток между слайдерами в px при разрешении 568px',
 									'value' => 30,
 									'empty' => true,
 									'options' => [
@@ -288,17 +423,17 @@ $review_meta_fields = [
 							]
 						],
 
-						'991' => [
+						'992' => [
 							'type'  => 'group',
-							'title' => '991',
-							'desc'  => 'Параметры 991',
+							'title' => '992',
+							'desc'  => 'Параметры 992',
 							// 'color' => '#0ff',
 							'value' => [
 								'slidesPerView' => [
 									'type'  => 'number',
 									'title' => 'Slider per view',
-									'desc'  => 'Количество видимых слайдов при разрешении 991px',
-									'value' => 1,
+									'desc'  => 'Количество видимых слайдов при разрешении 992px',
+									'value' => 2,
 									'empty' => true,
 									'options' => [
 										'step' => 1,
@@ -309,7 +444,40 @@ $review_meta_fields = [
 								'spaceBetween' => [
 									'type'  => 'number',
 									'title' => 'Space between',
-									'desc'  => 'Промежуток между слайдерами в px при разрешении 991px',
+									'desc'  => 'Промежуток между слайдерами в px при разрешении 992px',
+									'value' => 30,
+									'empty' => true,
+									'options' => [
+										'step' => 1,
+										'min'  => '0',
+										'max'  => ''
+									]
+								],
+							]
+						],
+
+						'1200' => [
+							'type'  => 'group',
+							'title' => '1200',
+							'desc'  => 'Параметры 1200',
+							// 'color' => '#0ff',
+							'value' => [
+								'slidesPerView' => [
+									'type'  => 'number',
+									'title' => 'Slider per view',
+									'desc'  => 'Количество видимых слайдов при разрешении 1200px',
+									'value' => 2,
+									'empty' => true,
+									'options' => [
+										'step' => 1,
+										'min'  => 1,
+										'max'  => ''
+									]
+								],
+								'spaceBetween' => [
+									'type'  => 'number',
+									'title' => 'Space between',
+									'desc'  => 'Промежуток между слайдерами в px при разрешении 1200px',
 									'value' => 30,
 									'empty' => true,
 									'options' => [
