6c1480318894af9139e490c58ad4f01ac8908264
Ravil <foragetonly800@mail.ru>
2022-12-22 06:58:12 +0300

    Add/Change - Добавил мета поля для заголовка и контента шаблона 'Stickers'. Теперь можно выбрать только одну картинку для фона контента и её позиционирование по умолчанию соответствует position: static.

--- extra/briz_tax_extra/meta/term/exports/category/stickers_meta_fields.php
+++ extra/briz_tax_extra/meta/term/exports/category/stickers_meta_fields.php
@@ -4,16 +4,67 @@
  */
 $stickers_meta_fields = [
 	'fields' => [
+		'header' => [
+			'type'  => 'checkbox',
+			'title' => 'Заголовок',
+			'desc'  => 'Показать/Спрятать заголовок.',
+			'empty' => true,
+			'value' => [ 'on' ],
+			'options' => [
+				'on' => 'On'
+			]
+		],
+		'header_first' => [
+			'type'  => 'text',
+			'title' => 'Заголовок секции первая часть',
+			'desc'  => 'Первая часть заголовка секции.',
+			'empty' => true,
+			'value' => 'Интересные'
+		],
+		'header_last' => [
+			'type'  => 'text',
+			'title' => 'Заголовок секции вторая часть',
+			'desc'  => 'Вторая часть заголовка секции.',
+			'empty' => true,
+			'value' => 'заметки'
+		],
+		'header_spacer' => [
+			'type'  => 'checkbox',
+			'title' => 'Header spacer',
+			'desc'  => 'Show/Hide header spacer.',
+			'empty' => true,
+			'value' => '',
+			'options' => [
+				'on' => 'On'
+			]
+		],
+		'header_description' => [
+			'type'  => 'checkbox',
+			'title' => 'Описание',
+			'desc'  => 'Показать/Спрятать описание.',
+			'empty' => true,
+			'value' => '',
+			'options' => [
+				'on' => 'On'
+			]
+		],
+		'header_description_text' => [
+			'type'  => 'textarea',
+			'title' => 'Текст описания',
+			'desc'  => 'Текст описания секции',
+			'empty' => true,
+			'value' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt deserunt iste, veniam necessitatibus repellat quis.'
+		],
 		'bg_img' => [
 			'type'  => 'media_button',
 			'title' => 'Background image',
 			'desc'  => '',
-			'value' => '[802]',
+			'value' => '[857]',
 			'empty' => false,
 			'options' => [
 				'title'    => 'Insert a media',
 				'library'  => [ 'type' => 'image' ],
-				'multiple' => true,
+				'multiple' => false,
 				'button'   => [ 'text' => 'Insert' ]
 			],
 		],
@@ -21,12 +72,54 @@ $stickers_meta_fields = [
 			'type'  => 'select',
 			'title' => 'Background attachment',
 			'desc'  => '',
-			'value' => 'fixed',
+			'value' => 'default',
 			'options' => [
 				'default'  => 'default',
 				'fixed'    => 'Fixed',
 				'parallax' => 'Parallax',
 				'hidden'   => 'Hidden'
+			]
+		],
+		'header_bg_color_enable' => [
+			'type'  => 'checkbox',
+			'title' => 'Показать цвет фона заголовка',
+			'desc'  => 'Показать цвет фона заголовка.',
+			'empty' => true,
+			'value' => '',
+			'options' => [
+				'on' => 'On'
+			]
+		],
+		'header_bg_color' => [
+			'type'  => 'color',
+			'title' => 'Цвет фона заголовка',
+			'desc'  => 'Цвет фона заголовка',
+			'value' => '#d9ffd1'
+		],
+		'content_bg_color_enable' => [
+			'type'  => 'checkbox',
+			'title' => 'Показать цвет фона контента',
+			'desc'  => 'Показать цвет фона контента.',
+			'empty' => true,
+			'value' => '',
+			'options' => [
+				'on' => 'On'
+			]
+		],
+		'content_bg_color' => [
+			'type'  => 'color',
+			'title' => 'Цвет фона контента',
+			'desc'  => 'Цвет фона контента',
+			'value' => '#b0c4de'
+		],
+		'content_wide' => [
+			'type'  => 'checkbox',
+			'title' => 'Широкий контент',
+			'desc'  => 'Контент на всю ширину экрана.',
+			'empty' => true,
+			'value' => '',
+			'options' => [
+				'on' => 'On'
 			]
 		],
 	]
