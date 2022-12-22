65508c3d249fa757736d7766c67594647b43f7b3
Ravil <foragetonly800@mail.ru>
2022-12-22 09:20:27 +0300

    Change - Изменил параметры слайдера в шаблоне 'Team'.

--- extra/briz_tax_extra/assets/js/briz_team.js
+++ extra/briz_tax_extra/assets/js/briz_team.js
@@ -1,57 +1,27 @@
 /**
  * Team functionality.
  */
-import { slider } from './export/slider.js';
+import { slider as s } from './export/slider.js';
 
 
 /**
  * Значения по умолчанию для слайдера.
  */
 const tmplSliderAtts = {
-  nextButton: '.swiper-button-next',
-  prevButton: '.swiper-button-prev',
-  pagination: '.swiper-pagination',
-  paginationClickable: true,
-  slidesPerView: 3,
-  spaceBetween: 30,
-  speed: 200,
-  breakpoints: {
-    250: {
-      slidesPerView: 1,
-      spaceBetween: 30,
-    },
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 30,
-    },
-    480: {
-      slidesPerView: 1,
-      spaceBetween: 30,
-    },
-    567: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-    991: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
+  navigation: {
+    nextEl: '.swiper-button-next-custom',
+    prevEl: '.swiper-button-prev-custom'
   },
 
+  pagination: {
+    // el: '.swiper-pagination-custom'
+  },
 
-  // Lazy loading
-  preloadImages: false,
-  lazyLoading: true,
-  lazyLoadingOnTransitionStart: true,
-  lazyLoadingInPrevNextAmount: 1
+  lazy: {
+    enabled: true,
+    loadOnTransitionStart: true,
+    preloaderClass: 'swiper-lazy-preloader-custom'
+  }
 };
 
-slider.init( '.briz-team-tmpl', tmplSliderAtts );
+s.init( '.briz-team-tmpl', tmplSliderAtts );
