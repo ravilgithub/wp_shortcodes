1618fe862f7089ec6b6c59439e322694a8b38b91
Ravil <foragetonly800@mail.ru>
2022-12-22 06:16:25 +0300

    Add/Refactoring - Добавил заголовок шаблона 'Services'. Подключил JS файл. Refactoring представления записи ( сервиса ).

--- extra/briz_tax_extra/tax_tmpl/services.php
+++ extra/briz_tax_extra/tax_tmpl/services.php
@@ -62,7 +62,9 @@ class Services {
 	 */
 	public function add_tmpl_assets() {
 		wp_enqueue_style( $this->tmpl_name . '-css' );
+		wp_enqueue_script( $this->tmpl_name . '-js' );
 	}
+
 
 	/**
 	 * Before content part of template.
@@ -81,16 +83,133 @@ class Services {
 	 */
 	public function get_before( $posts ) {
 		extract( $this->atts );
+
+		$meta_key = Helper::get_post_meta_key( __CLASS__, $posts[ 'data' ][ 0 ][ 'query' ] );
+		$opts = get_term_meta( $this->curr_term_id, $meta_key, true );
+		list( $bg, $attachment, $parallax_data, $parallax_img_src ) = Helper::get_bg_atts( $opts, true, 'bg_img', 'bg_attachment' );
+
+		$section_class = '';
+		$header = false;
+		$header_first = '';
+		$header_last = '';
+		$header_spacer = false;
+		$header_description = false;
+		$header_description_text = '';
+		$header_bg_color = '';
+		$content_bg_color = '';
+		$content_width_class = 'container';
+
+		if ( is_array( $opts ) ) {
+			if ( array_key_exists( 'header', $opts ) ) {
+				if ( $opts[ 'header' ] ) {
+					$header = true;
+					$section_class = 'section-with-header';
+				}
+			}
+
+			if ( array_key_exists( 'header_first', $opts ) ) {
+				$header_first = __( $opts[ 'header_first' ], $this->lang_domain );
+			}
+
+			if ( array_key_exists( 'header_last', $opts ) ) {
+				$header_last = __( $opts[ 'header_last' ], $this->lang_domain );
+			}
+
+			if ( array_key_exists( 'header_spacer', $opts ) ) {
+				$header_spacer = $opts[ 'header_spacer' ] ? true : $header_spacer;
+			}
+
+			if ( array_key_exists( 'header_description', $opts ) ) {
+				$header_description = $opts[ 'header_description' ] ? true : $header_description;
+			}
+
+			if ( array_key_exists( 'header_description_text', $opts ) ) {
+				$header_description_text = __( $opts[ 'header_description_text' ], $this->lang_domain );
+			}
+
+			if (
+				! empty( $opts[ 'header_bg_color_enable' ] ) &&
+				array_key_exists( 'header_bg_color_enable', $opts ) &&
+				array_key_exists( 'header_bg_color', $opts )
+			) {
+				$header_bg_color = 'background-color: ' . esc_attr( $opts[ 'header_bg_color' ] ) . ';';
+			}
+
+			if (
+				! $parallax_img_src &&
+				array_key_exists( 'content_bg_color_enable', $opts ) &&
+				! empty( $opts[ 'content_bg_color_enable' ] ) &&
+				array_key_exists( 'content_bg_color', $opts )
+			) {
+				$content_bg_color = ' background-color: ' . esc_attr( $opts[ 'content_bg_color' ] ) . ';';
+			}
+
+			if ( array_key_exists( 'content_wide', $opts ) ) {
+				$content_width_class = $opts[ 'content_wide' ] ? 'container-fluid' : $content_width_class;
+			}
+		}
 ?>
 		<section
 			id="<?php echo esc_attr( $this->id ); ?>"
-			class="<?php echo esc_attr( $this->tmpl_name ); ?> showcase section services-page <?php echo $class ?>"
+			class="showcase section services-page
+				<?php echo esc_attr( $this->tmpl_name ); ?>
+				<?php echo esc_attr( $class ); ?>
+				<?php echo $section_class; ?>
+			"
 			data-shortcode-term-id="<?php echo esc_attr( $this->curr_term_id ); ?>"
 		>
-			<div class="container">
-				<div class="row">
+<?php
+			if ( $header ) :
+?>
+				<div
+					class="section-caption-wrap"
+					style="<?php echo $header_bg_color; ?>"
+				>
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+<?php
+							if ( $header_first ||  $header_last ) :
+?>
+								<h2>
+									<?php echo $header_first; ?>
+									<span>
+										<?php echo $header_last; ?>
+									</span>
+								</h2>
+<?php
+							endif;
+							if ( $header_spacer ) :
+?>
+								<div class="briz-caption-spacer">
+									<div class="diamond"></div>
+								</div>
+<?php
+							endif;
+							if ( $header_description && $header_description_text ) :
+?>
+								<p><?php echo $header_description_text; ?></p>
+<?php
+							endif;
+?>
+							</div> <!-- .col- -->
+						</div> <!-- .row -->
+					</div> <!-- .container -->
+				</div> <!-- .section-caption-wrap -->
+<?php
+				endif;
+?>
+				<div
+					class="section-content-wrap <?php echo esc_attr( $attachment ); ?>"
+					style="<?php echo esc_attr( $bg ), $content_bg_color; ?>"
+					data-parallax="<?php echo esc_attr( $parallax_data ); ?>"
+					data-image-src="<?php echo esc_attr( $parallax_img_src ); ?>"
+				>
+					<div class="<?php echo $content_width_class; ?>">
+						<div class="row">
 <?php
 	}
+
 
 	/**
 	 * After content part of template.
@@ -109,11 +228,13 @@ class Services {
 	 */
 	public function get_after( $posts ) {
 ?>
-				</div> <!-- .row -->
-			</div> <!-- .container -->
+					</div> <!-- .row.services-items-wrap -->
+				</div> <!-- .container -->
+			</div> <!-- .section-content-wrap -->
 		</section> <!-- .briz-services-tmpl -->
 <?php
 	}
+
 
 	/**
 	 * Content of template.
@@ -143,28 +264,35 @@ class Services {
 				$post_id = get_the_id();
 				$meta_key = Helper::get_post_meta_key( __CLASS__, $query );
 				$opts = get_post_meta( $post_id, $meta_key, true );
+				$post_title = __( get_the_title(), $this->lang_domain );
+				$post_excerpt = __( get_the_excerpt(), $this->lang_domain );
+				$post_link = esc_url( get_permalink() );
+				$post_more_link_text = __( 'Readmore...', $this->lang_domain );
 ?>
-				<div
-					class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-0 services-item-wrap <?php echo esc_attr( $child->slug ); ?>"
-					data-post-id="<?php echo esc_attr( $post_id ); ?>"
-				>
+				<div class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-0 services-item-wrap">
 					<div class="icon-wrap">
-			<?php if ( array_key_exists( 'icon', $opts ) && $icon_name = $opts[ 'icon' ] ) : ?>
-							<i
-								class="fa fa-<?php echo esc_attr( $icon_name ); ?>"
-								aria-hidden="true"
-							></i>
-			<?php endif; ?>
+<?php
+					if ( array_key_exists( 'icon', $opts ) && $icon_name = esc_attr( $opts[ 'icon' ] ) ) :
+?>
+						<i
+							class="fa fa-<?php echo $icon_name; ?>"
+							aria-hidden="true"
+						></i>
+<?php
+					endif;
+?>
 					</div> <!-- .icon-wrap -->
 
 					<div class="content-wrap">
 						<h4>
-							<?php echo esc_attr( get_the_title() ); ?>
+							<?php echo $post_title; ?>
 						</h4>
-						<p><?php echo get_the_excerpt(); ?></p>
-						<a href="<?php esc_attr( the_permalink() ); ?>">Read more</a>
+						<p><?php echo $post_excerpt; ?></p>
+						<a href="<?php echo $post_link; ?>">
+							<?php echo $post_more_link_text; ?>
+						</a>
 					</div> <!-- .content-wrap -->
-				</div> <!-- .services-item-wrap -->
+				</div> <!-- .services-item -->
 <?php
 			endwhile;
 			wp_reset_postdata();
