17e54cb6f77ee348e2e2a08038c153b281920b89
Ravil <foragetonly800@mail.ru>
2023-02-05 21:32:57 +0300

    Add - Шаблон 'Products'.

--- extra/briz_tax_extra/tax_tmpl/products.php
+++ extra/briz_tax_extra/tax_tmpl/products.php
@@ -1,18 +1,429 @@
-<section id="products" class="<?php echo $class ?>">
-
 <?php
+	namespace Briz_Shortcodes\extra\briz_tax_extra\tax_tmpl;
 
-echo '<br />term id - '   . $term_id;
-echo '<br />term path - ' . $term_data[ 'tmpl_path' ];
-echo '<br />taxonomy - '  . $term_data[ 'taxonomy' ];
-echo '<br />operator - '  . $operator;
-echo '<br />children - '  . gettype( $children );
-echo '<br />limit - '     . $limit;
-echo '<br />offset - '    . $offset;
-echo '<br />orderby - '   . $orderby;
-echo '<br />order - '     . $order;
-echo '<br />meta key - '  . $meta_key;
+	if ( ! defined( 'ABSPATH' ) ) {
+		exit; // Exit if accessed directly
+	}
 
+	use Briz_Shortcodes\inc\Briz_Tax_Shortcode;
+	use Briz_Shortcodes\common\Helper;
+
+	use Briz_Shortcodes\extra\briz_tax_extra\tax_tmpl\related\products\Actions;
+
+	/**
+	 * Products template.
+	 *
+	 * Формирование вывода шаблона "products".
+	 *
+	 * @property String $tmpl_name        - имя шаблона.
+	 * @property String $content          - контент шорткода.
+	 * @property Array $atts              - параметры шорткода.
+	 * @property String $id               - id шорткода.
+	 * @property String $lang_domain      - id перевода шорткода.
+	 * @property Integer $curr_term_id    - id активного термина( tab'a ).
+	 * @property Integer $all_posts_count - количество записей термина или всех терминов.
+	 *
+	 * @since 0.0.1
+	 * @author Ravil
+	 */
+	class Products {
+		use Actions;
+
+		private $tmpl_name = 'briz-products-tmpl';
+		public $content;
+		public $atts;
+		public $id;
+		public $lang_domain;
+		public $curr_term_id;
+		public $all_posts_count = 0;
+
+
+		/**
+		 * Constructor
+		 *
+		 * @param String $content       - контент шорткода.
+		 * @param Array $atts           - параметры шорткода.
+		 * @param String $id            - id шорткода.
+		 * @param String $lang_domain   - id перевода шорткода.
+		 * @param Integer $curr_term_id - id активного термина( tab'a ).
+		 *
+		 * @return void
+		 *
+		 * @since 0.0.1
+		 * @author Ravil
+		 */
+		public function __construct( $content = '', $atts = [], $id = '', $lang_domain = '', $curr_term_id = null ) {
+			$this->content = $content;
+			$this->atts = $atts;
+			$this->id = $id;
+			$this->lang_domain = $lang_domain;
+			$this->curr_term_id = $curr_term_id;
+
+			$this->add_actions();
+			$this->set_filters();
+		}
+
+
+		/**
+		 * Adding previously registered styles and template scripts.
+		 *
+		 * Добавление ранее зарегистрированных стилей и скриптов шаблона.
+		 *
+		 * @return void
+		 *
+		 * @since 0.0.1
+		 * @author Ravil
+		 */
+		public function add_tmpl_assets() {
+			wp_enqueue_style( $this->tmpl_name . '-css' );
+			wp_enqueue_script( $this->tmpl_name . '-js' );
+		}
+
+
+		/**
+		 * Before content part of template.
+		 *
+		 * Часть шаблона выводимая перед контентом.
+		 *
+		 * @param Array $posts
+		 *  Array $posts[ 'atts' ]  - параметры шорткода.
+		 *  Array $posts[ 'child' ] - термин записи которого выводятся.
+		 *  Array $posts[ 'query' ] - WP Query Object.
+		 *
+		 * @return void
+		 *
+		 * @since 0.0.1
+		 * @author Ravil
+		 */
+		public function get_before( $posts ) {
+			extract( $this->atts );
+
+			$meta_key = Helper::get_post_meta_key( __CLASS__, $posts[ 'data' ][ 0 ][ 'query' ] );
+			$opts = get_term_meta( $this->curr_term_id, $meta_key, true );
+			list( $bg, $attachment, $parallax_data, $parallax_img_src ) = Helper::get_bg_atts( $opts, true, 'bg_img', 'bg_attachment' );
+
+			$section_class = '';
+			$header = false;
+			$header_first = '';
+			$header_last = '';
+			$header_spacer = false;
+			$header_description = false;
+			$header_description_text = '';
+			$header_bg_color = '';
+			$tab_bg_color = '';
+			$content_bg_color = '';
+			$content_width_class = 'container';
+
+			if ( is_array( $opts ) ) {
+				if ( array_key_exists( 'header', $opts ) ) {
+					if ( $opts[ 'header' ] ) {
+						$header = true;
+						$section_class = 'section-with-header';
+					}
+				}
+
+				if ( array_key_exists( 'header_first', $opts ) ) {
+					$header_first = __( $opts[ 'header_first' ], $this->lang_domain );
+				}
+
+				if ( array_key_exists( 'header_last', $opts ) ) {
+					$header_last = __( $opts[ 'header_last' ], $this->lang_domain );
+				}
+
+				if ( array_key_exists( 'header_spacer', $opts ) ) {
+					$header_spacer = $opts[ 'header_spacer' ] ? true : $header_spacer;
+				}
+
+				if ( array_key_exists( 'header_description', $opts ) ) {
+					$header_description = $opts[ 'header_description' ] ? true : $header_description;
+				}
+
+				if ( array_key_exists( 'header_description_text', $opts ) ) {
+					$header_description_text = __( $opts[ 'header_description_text' ], $this->lang_domain );
+				}
+
+				if (
+					! empty( $opts[ 'header_bg_color_enable' ] ) &&
+					array_key_exists( 'header_bg_color_enable', $opts ) &&
+					array_key_exists( 'header_bg_color', $opts )
+				) {
+					$header_bg_color = 'background-color: ' . esc_attr( $opts[ 'header_bg_color' ] ) . ';';
+				}
+
+				if (
+					! empty( $opts[ 'tab_bg_color_enable' ] ) &&
+					array_key_exists( 'tab_bg_color_enable', $opts ) &&
+					array_key_exists( 'tab_bg_color', $opts )
+				) {
+					$tab_bg_color = 'background-color: ' . esc_attr( $opts[ 'tab_bg_color' ] ) . ';';
+				}
+
+				if (
+					! $parallax_img_src &&
+					array_key_exists( 'content_bg_color_enable', $opts ) &&
+					! empty( $opts[ 'content_bg_color_enable' ] ) &&
+					array_key_exists( 'content_bg_color', $opts )
+				) {
+					$content_bg_color = ' background-color: ' . esc_attr( $opts[ 'content_bg_color' ] ) . ';';
+				}
+
+				if ( array_key_exists( 'content_wide', $opts ) ) {
+					$content_width_class = $opts[ 'content_wide' ] ? 'container-fluid' : $content_width_class;
+				}
+			}
 ?>
+			<section
+				id="<?php echo esc_attr( $this->id ); ?>"
+				class="showcase section products-page
+					<?php echo esc_attr( $this->tmpl_name ); ?>
+					<?php echo esc_attr( $class ); ?>
+					<?php echo $section_class; ?>"
+				data-shortcode-term-id="<?php echo esc_attr( $this->curr_term_id ); ?>"
+			>
+<?php
+			if ( $header ) :
+?>
+				<div
+					class="section-caption-wrap"
+					style="<?php echo $header_bg_color; ?>"
+				>
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+<?php
+							if ( $header_first ||  $header_last ) :
+?>
+								<h2>
+									<?php echo $header_first; ?>
+									<span>
+										<?php echo $header_last; ?>
+									</span>
+								</h2>
+<?php
+							endif;
+							if ( $header_spacer ) :
+?>
+								<div class="briz-caption-spacer">
+									<div class="diamond"></div>
+								</div>
+<?php
+							endif;
+							if ( $header_description && $header_description_text ) :
+?>
+								<p><?php echo $header_description_text; ?></p>
+<?php
+							endif;
+?>
+							</div> <!-- .col- -->
+						</div> <!-- .row -->
+					</div> <!-- .container -->
+				</div> <!-- .section-caption-wrap -->
+<?php
+			endif;
 
-</section>
+			// echo '<br />term id - '       . $this->curr_term_id;
+			// echo '<br />term name - '     . $term[ $this->curr_term_id ][ 'tmpl_name' ];
+			// echo '<br />term path - '     . $term[ $this->curr_term_id ][ 'tmpl_path' ];
+			// echo '<br />taxonomy - '      . $term[ $this->curr_term_id ][ 'taxonomy' ];
+			// echo '<br />operator - '      . $operator;
+			// echo '<br />self - '          . $self;
+			// echo '<br />children - '      . $children;
+			// echo '<br />grandchildren - ' . $grandchildren;
+			// echo '<br />limit - '         . $limit;
+			// echo '<br />offset - '        . $offset;
+			// echo '<br />orderby - '       . $orderby;
+			// echo '<br />order - '         . $order;
+			// echo '<br />meta key - '      . $meta_key;
+			// echo '<br />show more - '     . $show_more;
+
+			$childrens = Briz_Tax_Shortcode::get_term_childrens( $this->curr_term_id, $term[ $this->curr_term_id ][ 'taxonomy' ], $this->atts );
+
+			if ( $children || $grandchildren ) :
+?>
+				<div
+					class="section-tabs-wrap"
+					style="<?php echo $tab_bg_color; ?>"
+				>
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-12">
+								<ul class="tabs-list clearfix">
+<?php
+								$is_first = 'active';
+
+								foreach ( $childrens as $child ) :
+									$this->all_posts_count += $child->count;
+									$term_id = esc_attr( $child->term_id );
+									$term_posts_count = esc_attr( $child->count );
+									$term_slug = esc_attr( $child->slug );
+									$term_name = __( $child->name, $this->lang_domain );
+?>
+									<li
+										class="<?php echo $is_first; ?>"
+										data-term-id="<?php echo $term_id; ?>"
+										data-term-posts-count="<?php echo $term_posts_count; ?>"
+									>
+										<a href="#<?php echo $term_slug; ?>"><?php echo $term_name; ?></a>
+									</li>
+<?php
+									$is_first = '';
+								endforeach;
+?>
+								</ul> <!-- .isotop-filter -->
+							</div> <!-- .col-sm-12 -->
+						</div> <!-- .row -->
+					</div> <!-- .container -->
+				</div> <!-- .section-tabs-wrap -->
+<?php
+			else :
+				$this->all_posts_count = $childrens[ 0 ]->count;
+			endif;
+?>
+				<div
+					class="section-content-wrap <?php echo esc_attr( $attachment ); ?>"
+					style="<?php echo esc_attr( $bg ), $content_bg_color; ?>"
+					data-parallax="<?php echo esc_attr( $parallax_data ); ?>"
+					data-image-src="<?php echo esc_attr( $parallax_img_src ); ?>"
+				>
+					<div class="<?php echo $content_width_class; ?>">
+						<div class="row">
+							<div class="col-sm-12">
+<?php
+		}
+
+
+		/**
+		 * After content part of template.
+		 *
+		 * Часть шаблона выводимая после контента.
+		 *
+		 * @param Array $posts
+		 *  Array $posts[ 'atts' ]  - параметры шорткода.
+		 *  Array $posts[ 'child' ] - термин записи которого выводятся.
+		 *  Array $posts[ 'query' ] - WP Query Object.
+		 *
+		 * @return void
+		 *
+		 * @since 0.0.1
+		 * @author Ravil
+		 */
+		public function get_after( $posts ) {
+			extract( $this->atts );
+?>
+							</div> <!-- .col-sm-12 -->
+						</div> <!-- .row -->
+					</div> <!-- .container [ -fluid ] -->
+				</div> <!-- .section-content-wrap -->
+			</section> <!-- .briz-products-tmpl -->
+<?php
+		}
+
+
+		/**
+		 * Content of template.
+		 *
+		 * Контент шаблона.
+		 *
+		 * @param Array $posts
+		 *  Array $posts[ 'atts' ]  - параметры шорткода.
+		 *  Array $posts[ 'child' ] - термин записи которого выводятся.
+		 *  Array $posts[ 'query' ] - WP Query Object.
+		 *
+		 * @return void
+		 *
+		 * @since 0.0.1
+		 * @author Ravil
+		 */
+		public function get_content( $posts ) {
+			foreach ( $posts[ 'data' ] as $k => $data ) :
+				$child = $data[ 'child' ];
+				$query = $data[ 'query' ];
+
+				$id = esc_attr( $child->slug );
+				$class = ( 0 === $k ) ? 'active' : '';
+?>
+				<div id="<?php echo $id; ?>" class="tab-content-inner <?php echo $class; ?>">
+					<div class="swiper-navigation">
+						<div class="swiper-button-prev-custom"></div>
+						<div class="swiper-button-next-custom"></div>
+						<!-- <div class="swiper-pagination-custom"></div> -->
+					</div>
+
+					<div class="swiper">
+						<div class="swiper-wrapper">
+<?php
+						if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();
+							$post_id = get_the_id();
+							$term_slug = esc_attr( $child->slug );
+?>
+							<div class="swiper-slide">
+								<div class="<?php echo implode( ' ', get_post_class( 'product-thumb' ) ); ?>">
+									<div class="product-block-inner clearfix">
+<?php
+										// Image wrapper
+										do_action( 'shortcode_briz_tax_product_image_before' );
+											
+											// Link
+											do_action( 'shortcode_briz_tax_product_link_open' );
+											do_action( 'shortcode_briz_tax_product_image_preloader' );
+											do_action( 'shortcode_briz_tax_product_sale_flash' );
+											do_action( 'shortcode_briz_tax_product_message' );
+											do_action( 'shortcode_briz_tax_product_image' );
+											do_action( 'shortcode_briz_tax_product_link_close' );
+
+											// Buttons
+											do_action( 'shortcode_briz_tax_product_buttons_before' );
+											do_action( 'shortcode_briz_tax_product_add_to_cart' );
+											do_action( 'shortcode_briz_tax_product_add_to_wishlist' );
+											do_action( 'shortcode_briz_tax_product_add_to_compare' );
+											do_action( 'shortcode_briz_tax_product_quickview' );
+											do_action( 'shortcode_briz_tax_product_buttons_after' );
+
+										do_action( 'shortcode_briz_tax_product_image_after' );
+
+
+										// Caption
+										do_action( 'shortcode_briz_tax_product_caption_before' );
+
+											do_action( 'shortcode_briz_tax_product_caption_title' );
+
+										do_action( 'shortcode_briz_tax_product_caption_after' );
+?>
+									</div> <!-- .product-block-inner -->
+								</div> <!-- .product-thumb -->
+							</div> <!-- .swiper-slide -->
+<?php
+						endwhile;
+						wp_reset_postdata();
+						endif;
+?>
+						</div> <!-- .swiper-wrapper -->
+					</div> <!-- .swiper -->
+				</div> <!-- .tab-content-inner -->
+<?php
+			endforeach;
+		}
+
+
+		/**
+		 * 
+		 */
+		public function set_filters() {
+			add_filter( 'woocommerce_product_get_rating_html', [ $this, 'bri_create_empty_rating_stars' ], 10, 2 );
+		}
+
+
+		/**
+		 * Добавление пустых звёзд.
+		 * Нужно это потому, что если нет отзывов по умолчанию звёзды не показываются. 
+		 */
+		public function bri_create_empty_rating_stars( $rating_html, $rating ) {
+			if ( 0 >= $rating ) {
+				$rating = 0;
+				$rating_html = '<div class="star-rating" title="' . sprintf( esc_attr__( 'Rated %s out of 5', 'woocommerce' ), $rating ) . '">';
+				$rating_html .= '<span style="width:' . ( ( $rating / 5 ) * 100 ) . '%"><strong class="rating">' . $rating . '</strong> ' . esc_html__( 'out of 5', 'woocommerce' ) . '</span>';
+				$rating_html .= '</div>';
+			}
+			return $rating_html;
+		}
+	}
